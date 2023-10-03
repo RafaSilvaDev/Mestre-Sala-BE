f0da369e2741b55af420bac917c7b5097c1f6711
Rafael Barbosa <rafael.barbosa@encora.com>
2023-10-03 11:29:11 -0300

    Api working without CORS issues

--- src/main/java/com/project/mestresala/mestresalabe/config/CorsConfig.java
+++ src/main/java/com/project/mestresala/mestresalabe/config/CorsConfig.java
@@ -1,21 +1,17 @@
 package com.project.mestresala.mestresalabe.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*", "http://localhost:5173");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH");
     }
 }
--- src/main/java/com/project/mestresala/mestresalabe/controller/AuthenticationController.java
+++ src/main/java/com/project/mestresala/mestresalabe/controller/AuthenticationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthenticationController {
 
   @Autowired
